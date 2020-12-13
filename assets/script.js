f7ec30d09aae2b7fd82bb6c46e9ba57e3844e887
unknown <garthbhammonds@gmail.com>
2020-12-12 22:24:45 -0800

    cleaned up some code

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-        /* save text
-        onclick for button
-        save to local storage*/
+    /* save text
+    onclick for button
+    save to local storage*/
 
-    $('.saveBtn').on("click", function() {
+    $('.saveBtn').on("click", function () {
         const time = $(this).attr('id');
         const value = $(this).siblings('.description').val()
-        
+
         // Add inputs to local storage
         localStorage.setItem(time, value)
-    })   
+    })
 
-
+    // pulls value out of local storage and puts it in text area
     for (let i = 8; i <= 17; i++) {
         $(`#${i}`).siblings('textarea').val(localStorage.getItem(i));
     };
 
-
     function checkHour() {
         // Military
         const currentHour = moment().hours();
-
-        // Compare that current hour, to each hour block
-        // Change the backgorund color accordingly
-        // Hint: its already in the css file
-        // You will add / remove class based on <, =, > 
-        // .removeclass()
-        // .addCLass()
-
-    
+        
+        $("button").each(function() {
+            const elementHour = $(this).attr('id');
+            if (currentHour == elementHour) {
+                $(this).siblings("textarea").removeClass("past future")
+                $(this).siblings("textarea").addClass("present")
+                console.log("present");
+            }
+            else if (currentHour > elementHour) {
+                $(this).siblings("textarea").removeClass("present future")
+                $(this).siblings("textarea").addClass("past")
+                console.log("past");
+            }
+            else if (currentHour < elementHour) {
+                $(this).siblings("textarea").removeClass("past present")
+                $(this).siblings("textarea").addClass("future")
+                console.log("future");
+            }
+        })
     }
-
-
-
-
+    checkHour()
     $('#current-day').text(moment().format('dddd, MMMM Do'))
-
-
 })
